9d41328c7cd560e701943c05f9f8f644db058338
Jdavid765 <david.canoduran@hotmail.com>
2026-01-12 14:34:16 +0100

    new

--- algo/algo_small.c
+++ algo/algo_small.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 17:32:43 by david             #+#    #+#             */
-/*   Updated: 2026/01/11 18:50:49 by david            ###   ########.fr       */
+/*   Updated: 2026/01/12 14:32:56 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	algo_small(t_stack *stack)
 {
 	if (stack->a.size == 2)
 		for_two(stack);
-	if (stack->a.size == 3)
-		for_tree(stack);
 }
 
 void	for_two(t_stack *stack)
@@ -28,22 +26,3 @@ void	for_two(t_stack *stack)
 		ft_printf("All good nothing to change\n");
 }
 
-void	for_tree(t_stack *stack)
-{
-	int	i;
-	int	big;
-	int	position;
-
-	i = 0;
-	big = 0;
-	position = 0;
-	while (i < stack->a.size)
-	{
-		if (stack->a.list[i] > big)
-		{
-			big = stack->a.list[i];
-			position = i;
-		}
-		i++;
-	}
-}
