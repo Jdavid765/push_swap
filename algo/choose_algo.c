84fe81f7bee79e258696b8beaac8bdc15bcc7545
Jdavid765 <david.canoduran@hotmail.com>
2026-01-14 00:23:56 +0100

    new

--- algo/choose_algo.c
+++ algo/choose_algo.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 17:33:37 by david             #+#    #+#             */
-/*   Updated: 2026/01/11 18:15:59 by david            ###   ########.fr       */
+/*   Updated: 2026/01/13 17:19:40 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	choose_algo(t_stack *stack)
 {
 	if (stack->a.size <= 5)
 		algo_small(stack);
-	// else
-		// algo_big(stack);
+	else
+		algo_big(stack);
 }
